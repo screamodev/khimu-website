d5a0c1ebbc5b19e2a3fa530e49397a3656ff6d74
Your Name <you@example.com>
2026-05-29 12:51:12 +0000

    Add xhanges 29.05.26

---
+++ wp-content/plugins/cyr2lat/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'mihdan/cyr2lat',
+        'pretty_version' => '6.7.0',
+        'version' => '6.7.0.0',
+        'reference' => 'e88c8b72755e692ca2109d60dcb6df3d4b35ccf8',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'mihdan/cyr2lat' => array(
+            'pretty_version' => '6.7.0',
+            'version' => '6.7.0.0',
+            'reference' => 'e88c8b72755e692ca2109d60dcb6df3d4b35ccf8',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
---
+++ wp-content/plugins/pojo-accessibility/vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'name' => 'pojome/pojo-accessibility',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '6f76a0f64d06be7eb8f3573afd928ada5dea1d43',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'elementor/wp-notifications-package' => array(
+            'pretty_version' => '1.2.0',
+            'version' => '1.2.0.0',
+            'reference' => 'dd25ca9dd79402c3bb51fab112aa079702eb165e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../elementor/wp-notifications-package',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'elementor/wp-one-package' => array(
+            'pretty_version' => '1.0.54',
+            'version' => '1.0.54.0',
+            'reference' => null,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../elementor/wp-one-package',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'pojome/pojo-accessibility' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '6f76a0f64d06be7eb8f3573afd928ada5dea1d43',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
